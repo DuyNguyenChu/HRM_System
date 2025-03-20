56ce49f0a570e7f778d374144c27abdc484c534f
Quan Nguyen <quannguyen23.a@gmail.com>
2025-03-21 00:32:06 +0700

    Lưu thay đổi trước khi pull

--- DataAccessLayer/ActivityLogDao.cs
+++ DataAccessLayer/ActivityLogDao.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using BusinessObjects;
 using Microsoft.EntityFrameworkCore;
 
@@ -23,95 +22,6 @@ namespace DataAccessLayer
             return _context.ActivityLogs.Include(role => role.User).ToList();
         }
 
-        public static List<ActivityLog> GetActivityLog()
-        {
-            try
-            {
-                using var context = new HrmSystemContext();
-                return context.ActivityLogs.ToList();
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error: {ex.InnerException?.Message ?? ex.Message}", "Save Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                return new List<ActivityLog>();
-            }
-        }
-
-        public static List<ActivityLog> SearchActivityLog(string keyword)
-        {
-            try
-            {
-                using var context = new HrmSystemContext();
-
-                if (string.IsNullOrWhiteSpace(keyword))
-                {
-                    return context.ActivityLogs.ToList();
-                }
-
-                keyword = keyword.ToLower().Trim();
-                return context.ActivityLogs.Where(b =>
-                                    (b.Action != null && b.Action.ToLower().Contains(keyword)) ||
-                                    (b.TableName != null && b.TableName.ToLower().Contains(keyword)) ||
-                                    (b.TablePrimaryKey != null && b.TablePrimaryKey.ToLower().Contains(keyword)) ||
-                                    (b.Details != null && b.Details.ToLower().Contains(keyword)) ||
-                                    (b.RecordId.ToString().Contains(keyword)) ||
-                                    (b.Timestamp != null &&
-                                        (
-                                        b.Timestamp.Value.ToString("dd/MM/yyyy").Contains(keyword) ||
-                                        b.Timestamp.Value.Day.ToString().Contains(keyword) ||
-                                        b.Timestamp.Value.Month.ToString().Contains(keyword) ||
-                                        b.Timestamp.Value.Year.ToString().Contains(keyword)
-                                        )
-                                    )).ToList();
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error: {ex.InnerException?.Message ?? ex.Message}", "Save Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                return new List<ActivityLog>();
-            }
-        }
-        public static void AddActivityLog(ActivityLog p)
-        {
-            try
-            {
-                using var context = new HrmSystemContext();
-                context.ActivityLogs.Add(p);
-                MessageBox.Show("Hành động thêm đã được lưu");
-                context.SaveChanges();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error: {ex.InnerException?.Message ?? ex.Message}", "Save Error", MessageBoxButton.OK, MessageBoxImage.Error);
-
-
-            }
-        }
-        public static void DeleteActivityLog(ActivityLog p)
-        {
-            try
-            {
-                var context = new HrmSystemContext();
-                var DeleteContext = context.ActivityLogs.FirstOrDefault(c => c.ActivityLogId == p.ActivityLogId && c.UserId == p.UserId);
-                if (DeleteContext == null)
-                {
-                    MessageBox.Show("Không tìm thấy dữ liệu cần xóa");
-                }
-                else
-                {
-                    context.ActivityLogs.Remove(DeleteContext);
-                    MessageBox.Show("Hành động xóa đã được lưu");
-
-
-                    context.SaveChanges();
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error: {ex.InnerException?.Message ?? ex.Message}", "Save Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
 
     }
 }
