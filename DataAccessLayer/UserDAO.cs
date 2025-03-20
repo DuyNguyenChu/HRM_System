8a733c0adc093708f4ef64dbd75a9f00ab0773fe
vinh200187 <148328200+vinh200187@users.noreply.github.com>
2025-03-21 00:09:53 +0700

    login

---
+++ DataAccessLayer/UserDAO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects;
+
+namespace DataAccessLayer
+{
+    public class UserDAO
+    {
+   
+
+        public static User GetUserAcc(string phoneNumber, string passWord)
+        {
+            using var context = new HrmSystemContext();
+            var user = context.Users.FirstOrDefault(c => c.PhoneNumber.Equals(phoneNumber));
+
+            if (user != null && user.PasswordHash == passWord)
+            {
+                return user; 
+            }
+            return null; 
+        }
+    }
+}
---
+++ Services/UserServices.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects;
+using Repositories;
+
+namespace Services
+{
+    public class UserServices : IUserServices
+    {
+        private readonly IUserRepositories _userRepository;
+
+        public UserServices()
+        {
+            _userRepository = new UserRepositories();   
+        }
+
+        public User GetUserAcc(string phoneNumber, string passWord)
+        {
+           return _userRepository.GetUserAcc(phoneNumber, passWord);
+        }
+    }
+}
