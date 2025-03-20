086239f573d612a562381c72491d66435175bbf5
Quan Nguyen <quannguyen23.a@gmail.com>
2025-03-20 23:09:04 +0700

    pull main

--- BusinessObjects/obj/Debug/net8.0-windows/BusinessObjects.AssemblyInfo.cs
+++ BusinessObjects/obj/Debug/net8.0-windows/BusinessObjects.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessObjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bbff6b6f4cc3720f0a891b94172665aa94d620b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bd94d095810614585c27e43663feb15e7064b46")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessObjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessObjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
